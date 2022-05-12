5cc5bb008a128dd2046b70653cc6ec81c2a599a0
Jian Xue <royjianxue@yahoo.com>
2022-05-12 14:57:47 -0400

    Add project files.

---
+++ FirstClassAndMethodToString/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FirstClassAndMethodToString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person [] people = new Person[3]; //this initialized Person type array
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Please enter first name: ");
+                string input = Console.ReadLine();
+                people[i] = new Person()    // We instantiate the the variable that stores inside of array to be of a Person type, so we can access Name property
+                {
+                    Name = input
+                };   
+            }
+
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person);
+            }
+
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return $"Hello, My name is {Name}";
+        }
+
+    }
+
+
+}
